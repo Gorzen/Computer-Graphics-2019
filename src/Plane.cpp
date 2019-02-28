7c896f33e45ac7058d6e4d54a3386a51932c2bfd
Lucien Iseli <lucien.iseli93@gmail.com>
2019-02-28 15:02:16 +0100

    Fix raytracer plane

--- src/Plane.cpp
+++ src/Plane.cpp
@@ -42,16 +42,20 @@ intersect(const Ray& _ray,
      * - return whether there is an intersection in front of the viewer (t > 0)
     */
 
-   const double d = norm(center);
-   double t = dot(normal, (center-_ray.origin)) / dot(normal, _ray.direction);
+   double divisor = dot(normal, _ray.direction);
 
     _intersection_t = NO_INTERSECTION;
 
+    if(abs(divisor) < 1e-6)
+      return false;
+
+    double t = dot(normal, (center - _ray.origin)) / divisor;
+
     if(t < 0) return false;
     else {
-        _intersection_t = 1;
+        _intersection_t = t;
         _intersection_point = _ray(t);
-        _intersection_normal = _intersection_point - normal;
+        _intersection_normal = normal;
     }
     return true;
 }
