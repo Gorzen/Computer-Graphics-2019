c79ef276fbcecc45e40a00ced81c5acb113fe5b7
Lucien Iseli <lucien.iseli93@gmail.com>
2019-05-06 09:37:05 +0200

    Add loop LSystem, try track generation

--- project/LSystem/RollerCoasterLSystem.py
+++ project/LSystem/RollerCoasterLSystem.py
@@ -4,9 +4,12 @@ from .Symbol import Rules
 import math
 
 def get_points():
+    #loop = [track, p, track, track, track, p, track, track, track, p, track, track, track, p, track, track, track, p, track, track, track, p, track, track, track, p, track, track, track, p, track, track]
+
     straight = Symbol(" straight ")
     right = Symbol(" right ")
     f = Symbol("F")
+    track = Symbol(" track ")
     looping = Symbol(" looping ")
 
     p = Symbol("+")
@@ -16,11 +19,12 @@ def get_points():
 
     rules = Rules({right : [straight, p, straight],
                     looping : [straight, u, straight, u, straight, u, straight, u, m, m, m, m, straight, d, straight, d, straight, d, straight, d, straight, d, straight, d, straight, d, straight, d, p, p, p, p, straight, u, straight, u, straight, u, straight, u, straight],
-                    f : [f, p, m, u, f]})
+                    f : [f, p, m, u, f],
+                    track : [track, track, u, track, track, d, track, track, d, track, track, u]})
 
     lsystem = LSystem(rules, math.pi / 4, math.pi / 8, 1, (0.0, 0.0, 0.0))
 
-    symbols = lsystem.expand([looping], 10)
+    symbols = lsystem.expand([track, p, track, track, track, p, track, track, track, p, track, track, track, p, track, track, track, p, track, track, track, p, track, track, track, p, track, track, track, p, track, track], 1)
 
     pos = lsystem.compute_symbols(symbols)
 
