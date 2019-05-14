40aa7ca37e38db854a090e994941c0ffb4526f73
Lucien Iseli <lucien.iseli93@gmail.com>
2019-05-14 13:17:26 +0200

    Update LSystem rules

--- project/LSystem/RollerCoasterLSystem.py
+++ project/LSystem/RollerCoasterLSystem.py
@@ -19,13 +19,14 @@ def get_points(iterations):
     slope = Symbol(" slope ")
 
     t_s = Symbol(" track stochastic ", stochastic = True)
+    ts_small = Symbol(" small track ", stochastic = True)
     ts_0_0 = Symbol(" ss ", length = 5, stochastic = True)
     nasty_0_0 = Symbol(" nasty ")
 
     p = Symbol("+", angle = math.pi/4)
-	p3 = Symbol("+", angle = math.pi/3)
+    p3 = Symbol("+", angle = math.pi/3)
     m = Symbol("-", angle = math.pi/4)
-	m3 = Symbol("-", angle = math.pi/3)
+    m3 = Symbol("-", angle = math.pi/3)
     u = Symbol(" UP ", angle = math.pi/8)
     d = Symbol(" DOWN ", angle = math.pi/8)
     smooth_p = Symbol(" +++ ", angle = math.pi/2)
@@ -34,6 +35,9 @@ def get_points(iterations):
     loop_1 = [f1, p, f1, p, f1, p, f1, p, f1, p, f1, p, f1, p, f1]
 
     loop_ts_s = [f5, p, f5, p, ts_0_0, p, f5, p, f5, p, ts_0_0, p, f5, p, f5]
+
+    loop_ts_s_u = [u, f5, p, f5, p, d, ts_0_0, u, p, f5, d, d, p, f5, p, u, ts_0_0, d, p, f5, p, f5]
+
 
     loop = [track, p, track, track, track, p, track, track, track, p, track, track, track, p, track, track, track, p, track, track, track, p, track, track, track, p, track, track, track, p, track, track]
 
@@ -46,12 +50,13 @@ def get_points(iterations):
     rules = Rules({right : [f1, smooth_p, f1],
                     looping : [f1, u, f1, u, f1, u, f1, u, m, m, m, m, f1, d, f1, d, f1, d, f1, d, f1, d, f1, d, f1, d, f1, d, p, p, p, p, f1, u, f1, u, f1, u, f1, u, f1],
                     slope : [track, track, u, track, track, d, track, track, d, track, track, u],
-                    ts_0_0 : [[0.25, [p, p, u, f10, d, m, m, ts_0_0, m, m, d, f10, u, p, p]],
-                            [0.25, [p, f1, p, p, f1, p, f1, m, f1, m, ts_0_0, m, f1, m, f1, ts_0_0, f1, m, f1, f1, m, ts_0_0, m, f1, m, f1, p, f1, p, p, p]],
-                            [0.25, [p, p, ts_0_0, m, m, ts_0_0, m, m, ts_0_0, p, p]],
-                            [0.25, [m, m, ts_0_0, p, p, ts_0_0, p, p, ts_0_0, m, m]]],
-					ts_small : [[0.33, [p3, f1, f1, f1, f1, m3, f1, f1, f1, m3, f1, f1, f1, f1, p3]],
-							[0.33, [m3, f1, f1, f1, f1, p3, f1, f1, f1, p3, f1, f1, f1, f1, m3]],
+                    ts_0_0 : [[0.15, [p, p, u, f10, d, m, m, ts_0_0, m, m, d, f10, u, p, p]],
+                            [0.15, [p, p, d, f10, u, m, m, ts_0_0, m, m, u, f10, d, p, p]],
+                            [0.1, [p, f1, p, p, f1, p, f1, m, f1, m, ts_0_0, m, f1, m, f1, ts_0_0, f1, m, f1, f1, m, ts_0_0, m, f1, m, f1, p, f1, p, p, p]],
+                            [0.3, [p, p, ts_0_0, m, m, ts_0_0, m, m, ts_0_0, p, p]],
+                            [0.3, [m, m, ts_0_0, p, p, ts_0_0, p, p, ts_0_0, m, m]]],
+					ts_small : [[0.33, [p3, f1, f1, f1, f1, m3, f1, m3, f1, f1, f1, f1, p3]],
+							[0.33, [m3, f1, f1, f1, f1, p3, f1, p3, f1, f1, f1, f1, m3]],
 							[0.34, [f1, f1, f1, f1, f1]]],
                     nasty_0_0 : [p, f1, p, p, f1, p, f1, m, f1, m, ts_0_0, m, f1, m, f1, f1, f1, m, f1, f1, m, f1, m, f1, m, f1, p, f1, p, p, p],
                     t_s :   [[0.1428, [t_s, p, t_s, m]],
@@ -65,7 +70,7 @@ def get_points(iterations):
     lsystem = LSystem(rules, (0.0, 0.0, 0.0))
 
     #symbols = lsystem.expand([p, p, f1, m, m, f1, m, m, f1, p, p], 3)
-    symbols = lsystem.expand(loop_ts_s, iterations)
+    symbols = lsystem.expand(loop_ts_s_u, iterations)
 
     pos = lsystem.compute_symbols(symbols)
 
