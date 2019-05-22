e1fa10770e8d092bb79f3915a04f209fc38e2605
Lucien Iseli <lucien.iseli93@gmail.com>
2019-05-22 16:14:27 +0200

    Add buttons to Blender RollerCoaster

--- project/LSystem/RollerCoasterLSystem.py
+++ project/LSystem/RollerCoasterLSystem.py
@@ -8,105 +8,113 @@ except:
     from Symbol import Symbol
 import math
 
-def get_points(iterations, realistic_twisting):
-    ### Old parts
-    #fun_twist = [u, p, f1, p, f1, p, f1, p, f1, p, f1, p, f1, p, f1, p, f1, d, ts_0_0, d, f1, m, f1, m, f1, m, f1, m, f1, m, f1, m, f1, m, f1, m, f1]
-    #fun_twist_samer = [u, p, f1, p, f1, p, f1, p, f1, p, f1, p, f1, p, f1, p, f1, p, f1, p, f1, p, f1, p, f1, f1, p, f1, p, f1, p, f1, p, f1, p, f1, p, f1, p, f1, p, f1, p, f1, p, f1, p, f1, p, f1, f1, d, f1, d, f1, m, f1, m, f1, m, f1, m, f1, m, f1, m, f1, m, f1, m, f1, m, f1, m, f1, m, f1, m, f1, p, f1, f1, f1, p, f1, f1, f1]
+### Old parts
+#fun_twist = [u, p, f1, p, f1, p, f1, p, f1, p, f1, p, f1, p, f1, p, f1, d, ts_0_0, d, f1, m, f1, m, f1, m, f1, m, f1, m, f1, m, f1, m, f1, m, f1]
+#fun_twist_samer = [u, p, f1, p, f1, p, f1, p, f1, p, f1, p, f1, p, f1, p, f1, p, f1, p, f1, p, f1, p, f1, f1, p, f1, p, f1, p, f1, p, f1, p, f1, p, f1, p, f1, p, f1, p, f1, p, f1, p, f1, p, f1, f1, d, f1, d, f1, m, f1, m, f1, m, f1, m, f1, m, f1, m, f1, m, f1, m, f1, m, f1, m, f1, m, f1, m, f1, p, f1, f1, f1, p, f1, f1, f1]
 
 
-    # ALL SYMBOLS
-    ## Straight symbols
-    f1 = Symbol("F", length = 1)
-    f2 = Symbol("F", length = 2)
-    f3 = Symbol("F", length = 3)
-    f5 = Symbol("F", length = 5)
-    f10 = Symbol("F", length = 10)
+# ALL SYMBOLS
+## Straight symbols
+f1 = Symbol("F", length = 1)
+f2 = Symbol("F", length = 2)
+f3 = Symbol("F", length = 3)
+f5 = Symbol("F", length = 5)
+f10 = Symbol("F", length = 10)
 
-    ## Twists
-    twist_p_5 = Symbol(" TWIST ^ ", length = 5)
-    twist_p_10 = Symbol(" TWIST ^ ", length = 10)
-    twist_m_5 = Symbol(" TWIST v ", length = 5)
-    twist_m_10 = Symbol(" TWIST v ", length = 10)
-    semi_twist_p = Symbol(" SEMI TWIST ^ ", length = 5)
-    semi_twist_m = Symbol(" SEMI TWIST  v ", length = 5)
+## Twists
+twist_p_5 = Symbol(" TWIST ^ ", length = 5)
+twist_p_10 = Symbol(" TWIST ^ ", length = 10)
+twist_m_5 = Symbol(" TWIST v ", length = 5)
+twist_m_10 = Symbol(" TWIST v ", length = 10)
+semi_twist_p = Symbol(" SEMI TWIST ^ ", length = 5)
+semi_twist_m = Symbol(" SEMI TWIST  v ", length = 5)
 
-    ## Stochastic track
-    ts_0_0 = Symbol(" ss ", length = 5, stochastic = True)
-    ts_p = Symbol(" ss ", length = 5, stochastic = True)
-    ts_m = Symbol(" ss ", length = 5, stochastic = True)
-    ts_squares_p = Symbol(" ss ", length = 5, stochastic = True)
-    ts_squares_m = Symbol(" ss ", length = 5, stochastic = True)
+## Stochastic track
+ts_0_0 = Symbol(" ss ", length = 5, stochastic = True)
+ts_p = Symbol(" ss ", length = 5, stochastic = True)
+ts_m = Symbol(" ss ", length = 5, stochastic = True)
+ts_squares_p = Symbol(" ss ", length = 5, stochastic = True)
+ts_squares_m = Symbol(" ss ", length = 5, stochastic = True)
 
-    ## Turning symbols, theta and phi
-    p = Symbol("+", angle = math.pi/4)
-    p3 = Symbol("+", angle = math.pi/3)
-    m = Symbol("-", angle = math.pi/4)
-    m3 = Symbol("-", angle = math.pi/3)
-    u = Symbol(" UP ", angle = math.pi/8)
-    u3 = Symbol(" UP ", angle = math.pi/3)
-    d = Symbol(" DOWN ", angle = math.pi/8)
-    d3 = Symbol(" DOWN ", angle = math.pi/3)
+## Turning symbols, theta and phi
+p = Symbol("+", angle = math.pi/4)
+p3 = Symbol("+", angle = math.pi/3)
+m = Symbol("-", angle = math.pi/4)
+m3 = Symbol("-", angle = math.pi/3)
+u = Symbol(" UP ", angle = math.pi/8)
+u3 = Symbol(" UP ", angle = math.pi/3)
+d = Symbol(" DOWN ", angle = math.pi/8)
+d3 = Symbol(" DOWN ", angle = math.pi/3)
 
-    # RULES
-    ## Starting loops
-    loop_ts_s_u = [u, f5, p, f5, p, d, ts_0_0, u, p, f5, d, d, p, f5, p, u, ts_0_0, d, p, f5, p, f5]
-    loop_ts_s_u_8 = [f2, u, f5, p, f5, p, d, ts_0_0, d, p, f5, p, f5, u, ts_0_0, f2, u, f5, p, f5, p, d, ts_0_0, d, p, f5, p, f5, u, ts_0_0]
+# RULES
+## Starting loops
+loop_ts_s_u = [u, f5, p, f5, p, d, ts_0_0, u, p, f5, d, d, p, f5, p, u, ts_0_0, d, p, f5, p, f5]
+loop_ts_s_u_8 = [f2, u, f5, p, f5, p, d, ts_0_0, d, p, f5, p, f5, u, ts_0_0, f2, u, f5, p, f5, p, d, ts_0_0, d, p, f5, p, f5, u, ts_0_0]
 
-    ## Expansion rules
-    looping = [f1, f1, f1, u, f1, u, f1, u, f1, u, m, m, m, m, f1, d, f1, d, f1, d, f1, d, f1, d, f1, d, f1, d, f1, d, p, p, p, p, f1, u, f1, u, f1, u, f1, u, f1, f1, f1]
-    up_turn_p = [p, p, u, f10, d, m, m, ts_0_0, m, m, d, f10, u, p, p]
-    up_turn_m = [m, m, u, f10, d, p, p, ts_0_0, p, p, d, f10, u, m, m]
-    down_turn_p = [p, p, d, f10, u, m, m, ts_0_0, m, m, u, f10, d, p, p]
-    down_turn_m = [m, m, d, f10, u, p, p, ts_0_0, p, p, u, f10, d, m, m]
-    square_p = [p, f1, p, p, f1, m, ts_m, f2, m, m, ts_p, m, m, f2, ts_p, m, f1, p, p, f1, p]
-    square_m = [m, f1, m, m, f1, p, ts_p, f2, p, p, ts_m, p, p, f2, ts_m, p, f1, m, m, f1, m]
-    small_square_p = [p, p, ts_m, f2, m, m, ts_p, m, m, f2, ts_p, p, p]
-    small_square_m = [m, m, ts_p, f2, p, p, ts_p, p, p, f2, ts_p, m, m]
-    final_turn_p = [p3, f3, m3, f2, m3, f3, p3]
-    final_turn_m = [m3, f3, p3, f2, p3, f3, m3]
-    final_slope = [u3, f3, d3, f2, d3, f3, u3]
-    nothing = [ts_0_0]
-    fun_twist = [u, p, f1, p, f1, p, f1, p, f1, p, f1, p, f1, p, f1, p, f1, d, ts_0_0, d, f1, m, f1, m, f1, m, f1, m, f1, m, f1, m, f1, m, f1, m, u]
-    fun_twist_2 = [u, p, f1, p, f1, p, f1, p, f1, p, f1, p, f1, p, f1, p, f1, p, f1, p, f1, p, f1, p, f1, p, f1, p, f1, p, f1, p, f1, d, ts_0_0, d, f1, m, f1, m, f1, m, f1, m, f1, m, f1, m, f1, m, f1, m, f1, m, f1, m, f1, m, f1, m, f1, m, f1, m, f1, m, f1, m, u]
+## Expansion rules
+looping = [f1, f1, f1, u, f1, u, f1, u, f1, u, m, m, m, m, f1, d, f1, d, f1, d, f1, d, f1, d, f1, d, f1, d, f1, d, p, p, p, p, f1, u, f1, u, f1, u, f1, u, f1, f1, f1]
+up_turn_p = [p, p, u, f10, d, m, m, ts_0_0, m, m, d, f10, u, p, p]
+up_turn_m = [m, m, u, f10, d, p, p, ts_0_0, p, p, d, f10, u, m, m]
+down_turn_p = [p, p, d, f10, u, m, m, ts_0_0, m, m, u, f10, d, p, p]
+down_turn_m = [m, m, d, f10, u, p, p, ts_0_0, p, p, u, f10, d, m, m]
+square_p = [p, f1, p, p, f1, m, ts_m, f2, m, m, ts_p, m, m, f2, ts_p, m, f1, p, p, f1, p]
+square_m = [m, f1, m, m, f1, p, ts_p, f2, p, p, ts_m, p, p, f2, ts_m, p, f1, m, m, f1, m]
+small_square_p = [p, p, ts_m, f2, m, m, ts_p, m, m, f2, ts_p, p, p]
+small_square_m = [m, m, ts_p, f2, p, p, ts_p, p, p, f2, ts_p, m, m]
+final_turn_p = [p3, f3, m3, f2, m3, f3, p3]
+final_turn_m = [m3, f3, p3, f2, p3, f3, m3]
+final_slope = [u3, f3, d3, f2, d3, f3, u3]
+nothing = [ts_0_0]
+fun_twist = [u, p, f1, p, f1, p, f1, p, f1, p, f1, p, f1, p, f1, p, f1, d, ts_0_0, d, f1, m, f1, m, f1, m, f1, m, f1, m, f1, m, f1, m, f1, m, u]
+fun_twist_2 = [u, p, f1, p, f1, p, f1, p, f1, p, f1, p, f1, p, f1, p, f1, p, f1, p, f1, p, f1, p, f1, p, f1, p, f1, p, f1, p, f1, d, ts_0_0, d, f1, m, f1, m, f1, m, f1, m, f1, m, f1, m, f1, m, f1, m, f1, m, f1, m, f1, m, f1, m, f1, m, f1, m, f1, m, f1, m, u]
 
-    rules = Rules({ts_0_0 : [[0.35, [ts_p]],
-                            [0.35, [ts_m]],
-							[0.15, [ts_squares_p]],
-							[0.15, [ts_squares_m]]],
-					ts_p : [[0.15, up_turn_p],
-                            [0.15, down_turn_p],
-                            [0.05, looping],
-							[0.2, fun_twist],
-							[0.075, fun_twist_2],
-                            [0.075, final_turn_p],
-                            [0.075, final_slope],
-        					[0.04, [twist_p_5]],
-                            [0.04, [twist_m_5]],
-                            [0.06, [semi_twist_p]],
-                            [0.06, [semi_twist_m]],
-                            [0.025, nothing]],
-					ts_m : [[0.15, up_turn_m],
-                            [0.15, down_turn_m],
-                            [0.05, looping],
-							[0.2, fun_twist],
-							[0.075, fun_twist_2],
-        					[0.075, final_turn_m],
-                            [0.075, final_slope],
-        					[0.04, [twist_p_5]],
-                            [0.04, [twist_m_5]],
-                            [0.06, [semi_twist_p]],
-                            [0.06, [semi_twist_m]],
-                            [0.025, nothing]],
-					ts_squares_p : [[0.5, square_p],
-                            [0.5, small_square_p]],
-					ts_squares_m : [[0.5, square_m],
-							[0.5, small_square_m]]})
+rules = Rules({ts_0_0 : [[0.35, [ts_p]],
+                        [0.35, [ts_m]],
+                        [0.15, [ts_squares_p]],
+                        [0.15, [ts_squares_m]]],
+                ts_p : [[0.15, up_turn_p],
+                        [0.15, down_turn_p],
+                        [0.05, looping],
+                        [0.2, fun_twist],
+                        [0.075, fun_twist_2],
+                        [0.075, final_turn_p],
+                        [0.075, final_slope],
+                        [0.04, [twist_p_5]],
+                        [0.04, [twist_m_5]],
+                        [0.06, [semi_twist_p]],
+                        [0.06, [semi_twist_m]],
+                        [0.025, nothing]],
+                ts_m : [[0.15, up_turn_m],
+                        [0.15, down_turn_m],
+                        [0.05, looping],
+                        [0.2, fun_twist],
+                        [0.075, fun_twist_2],
+                        [0.075, final_turn_m],
+                        [0.075, final_slope],
+                        [0.04, [twist_p_5]],
+                        [0.04, [twist_m_5]],
+                        [0.06, [semi_twist_p]],
+                        [0.06, [semi_twist_m]],
+                        [0.025, nothing]],
+                ts_squares_p : [[0.5, square_p],
+                        [0.5, small_square_p]],
+                ts_squares_m : [[0.5, square_m],
+                        [0.5, small_square_m]]})
 
-    start = loop_ts_s_u_8
+start = loop_ts_s_u_8
 
-    lsystem = LSystem(rules)
+lsystem = LSystem(rules)
+
+def get_points(iterations):
     symbols = lsystem.expand(start, iterations)
     pos = lsystem.compute_symbols(symbols)
-    if (realistic_twisting):
-        pos = lsystem.twist_points(pos)
-    return pos
+    twisted_pos = lsystem.twist_points(pos)
+
+    return (symbols, pos, twisted_pos)
+
+def one_iteration(symbols):
+    symbols = lsystem.expand(symbols, 1)
+    pos = lsystem.compute_symbols(symbols)
+    twisted_pos = lsystem.twist_points(pos)
+
+    return (symbols, pos, twisted_pos)
--- project/RollerCoaster.py
+++ project/RollerCoaster.py
@@ -21,6 +21,7 @@ import LSystem as LS
 
 from LSystem.LSystems import *
 from LSystem.Symbol import *
+from LSystem.GlobalVariables import vars
 from LSystem.RollerCoasterLSystem import *
 
 importlib.reload(LS.LSystems)
@@ -59,9 +60,6 @@ class RollerCoasterPanel(Panel):
         row.label(text='Roller Coaster features')
 
         row = layout.row()
-        row.prop(settings, 'twisting', text='Realistic tilting')
-
-        row = layout.row()
         row.prop(settings, 'iterations', text = 'Number of iterations')
 
         row = layout.row()
@@ -70,62 +68,104 @@ class RollerCoasterPanel(Panel):
         row = layout.row()
         self.layout.operator('roller_coaster.delete_roller_coaster', text='Delete the Roller Coaster')
 
+        row = layout.row()
+        self.layout.operator('roller_coaster.twist_tracks', text='Twist!')
+
+        row = layout.row()
+        self.layout.operator('roller_coaster.expand_tracks', text='Expand once!')
+
+
+def draw_tracks_roller_coaster(points):
+    curveName = 'RollerCoasterCurve'
+    tracksName = 'tracks'
+
+    #First delete the existing roller coaster if it exists
+    bpy.ops.roller_coaster.delete_roller_coaster()
+
+    #Draw the curve
+    curveData = bpy.data.curves.new('trackCurve', type='CURVE')
+    curveData.dimensions = '3D'
+    curveData.resolution_u = 2
+
+    curve = curveData.splines.new('NURBS')
+    curve.use_endpoint_u = True
+    curve.points.add(len(points)-1)
+    for i, coord in enumerate(points):
+        x,y,z,t = coord
+        curve.points[i].co = (x, y, z, 1)
+        curve.points[i].tilt = t
+
+    curveObject = bpy.data.objects.new(curveName, curveData)
+    scn = bpy.context.scene
+    scn.objects.link(curveObject)
+    scn.objects.active = curveObject
+    curveObject.select = True
+    bpy.context.object.data.splines[0].order_u = 3
+    bpy.context.object.data.splines[0].use_cyclic_u = True
+    bpy.context.object.data.twist_mode = 'Z_UP'
+    bpy.context.object.data.use_deform_bounds = True
+
+    #Draw the tracks
+    #import the tracks and set it active
+    bpy.ops.object.select_all(action='DESELECT')
+    path = os.path.join(dir, 'tracks.obj')
+    tracks = bpy.ops.import_scene.obj(filepath = path)
+    bpy.context.scene.objects.active = bpy.context.selected_objects[0]
+    bpy.context.object.name = tracksName
+    #Add array modifier to the tracks
+    bpy.ops.object.modifier_add(type='ARRAY')
+    bpy.context.object.modifiers["Array"].fit_type = 'FIT_CURVE'
+    bpy.context.object.modifiers["Array"].curve = bpy.data.objects[curveName]
+    bpy.ops.object.modifier_add(type='CURVE')
+    bpy.context.object.modifiers["Curve"].object = bpy.data.objects[curveName]
+
+
+class track_expander_operator(bpy.types.Operator):
+    bl_idname = 'roller_coaster.expand_tracks'
+    bl_label = 'Expand roller coaster tracks'
+    bl_options = {"REGISTER", "UNDO"}
+
+    def execute(self, context):
+        if len(vars.symbols) > 0:
+            vars.symbols, vars.points, vars.twisted_points = one_iteration(vars.symbols)
+
+            draw_tracks_roller_coaster(vars.points)
+
+        return {"FINISHED"}
+
+
+class track_twister_operator(bpy.types.Operator):
+    bl_idname = 'roller_coaster.twist_tracks'
+    bl_label = 'Twist roller coaster tracks'
+    bl_options = {"REGISTER", "UNDO"}
+
+    def execute(self, context):
+        if len(vars.points) > 0:
+            vars.twisted = not vars.twisted
+            if vars.twisted:
+                print("Drawing twisted points")
+                draw_tracks_roller_coaster(vars.twisted_points)
+            else:
+                print("Drawing untwisted points")
+                draw_tracks_roller_coaster(vars.points)
+
+        return {"FINISHED"}
+
+
 class track_generator_operator(bpy.types.Operator):
     bl_idname = 'roller_coaster.draw_tracks'
     bl_label = 'Draw roller coaster tracks'
     bl_options = {"REGISTER", "UNDO"}
 
     def execute(self, context):
-
-    #Get settings
+        #Get settings
         settings = context.scene.roller_coaster_settings
         iterations = settings.iterations
-        realistic_twisting = settings.twisting
 
-        points = get_points(iterations, realistic_twisting)
+        vars.symbols, vars.points, vars.twisted_points = get_points(iterations)
 
-        curveName = 'RollerCoasterCurve'
-        tracksName = 'tracks'
+        draw_tracks_roller_coaster(vars.points)
 
-    #First delete the existing roller coaster if it exists
-        bpy.ops.roller_coaster.delete_roller_coaster()
-
-    #Draw the curve
-        curveData = bpy.data.curves.new('trackCurve', type='CURVE')
-        curveData.dimensions = '3D'
-        curveData.resolution_u = 2
-
-        curve = curveData.splines.new('NURBS')
-        curve.use_endpoint_u = True
-        curve.points.add(len(points)-1)
-        for i, coord in enumerate(points):
-            x,y,z,t = coord
-            curve.points[i].co = (x, y, z, 1)
-            curve.points[i].tilt = t
-
-        curveObject = bpy.data.objects.new(curveName, curveData)
-        scn = bpy.context.scene
-        scn.objects.link(curveObject)
-        scn.objects.active = curveObject
-        curveObject.select = True
-        bpy.context.object.data.splines[0].order_u = 3
-        bpy.context.object.data.splines[0].use_cyclic_u = True
-        bpy.context.object.data.twist_mode = 'Z_UP'
-        bpy.context.object.data.use_deform_bounds = True
-
-    #Draw the tracks
-        #import the tracks and set it active
-        bpy.ops.object.select_all(action='DESELECT')
-        path = os.path.join(dir, 'tracks.obj')
-        tracks = bpy.ops.import_scene.obj(filepath = path)
-        bpy.context.scene.objects.active = bpy.context.selected_objects[0]
-        bpy.context.object.name = tracksName
-        #Add array modifier to the tracks
-        bpy.ops.object.modifier_add(type='ARRAY')
-        bpy.context.object.modifiers["Array"].fit_type = 'FIT_CURVE'
-        bpy.context.object.modifiers["Array"].curve = bpy.data.objects[curveName]
-        bpy.ops.object.modifier_add(type='CURVE')
-        bpy.context.object.modifiers["Curve"].object = bpy.data.objects[curveName]
         #Indicate the script terminated successfully
         return {"FINISHED"}
 
@@ -154,6 +194,8 @@ def register():
     bpy.utils.register_class(RollerCoasterPanel)
     bpy.utils.register_class(track_generator_operator)
     bpy.utils.register_class(delete_operator)
+    bpy.utils.register_class(track_twister_operator)
+    bpy.utils.register_class(track_expander_operator)
     bpy.utils.register_class(RollerCoasterSettings)
     bpy.types.Scene.roller_coaster_settings = PointerProperty(type=RollerCoasterSettings)
 
@@ -161,6 +203,8 @@ def unregister():
     bpy.utils.unregister_class(RollerCoasterPanel)
     bpy.utils.unregister_class(track_generator_operator)
     bpy.utils.unregister_class(delete_operator)
+    bpy.utils.unregister_class(track_twister_operator)
+    bpy.utils.unregister_class(track_expander_operator)
     del bpy.types.Scene.roller_coaster_settings
 
 
